e02dd3143508e60817510afcf44874998d8a4f05
Preri <44541362+prerikoth@users.noreply.github.com>
2020-03-18 18:37:32 +0200

    Added 8th Exercise

---
+++ Python/8 Pirates of the Biss/pirates.py
@@ -0,0 +1,7 @@
+def dejumble(jumbled_word, words):
+    "Recieves a jumbled_word and a list of fixed words. The function returns the dejumbled word"
+
+    words_new = [sorted(word) for word in words]
+    return [words[i] for i, word in enumerate(words_new) if sorted(word) == sorted(jumbled_word)]
+
+print(dejumble('ortsp', ['sport', 'parrot', 'ports', 'matey']))
