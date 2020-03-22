ab0d996905aa855edc3097d7316e98955da892f0
Preri <44541362+prerikoth@users.noreply.github.com>
2020-03-22 17:58:32 +0200

    Fixed input mistake

--- Python/8 Pirates of the Biss/pirates.py
+++ Python/8 Pirates of the Biss/pirates.py
@@ -2,6 +2,6 @@ def dejumble(jumbled_word, words):
     "Recieves a jumbled_word and a list of fixed words. The function returns the dejumbled word"
 
     words_new = [sorted(word) for word in words]
-    return [words[i] for i, word in enumerate(words_new) if sorted(word) == sorted(jumbled_word)]
+    return [words[i] for i, word in enumerate(words_new) if word == sorted(jumbled_word)]
 
 print(dejumble('ortsp', ['sport', 'parrot', 'ports', 'matey']))
